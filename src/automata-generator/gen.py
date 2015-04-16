c344d0c56b9c7817b04e9370cffb1c3fce2c421c
Mateus Zitelli <zitellimateus@gmail.com>
2015-04-16 17:16:02 -0300

    Make the automata generator moe powerfull (ugly).

--- src/automata-generator/gen.py
+++ src/automata-generator/gen.py
@@ -6,29 +6,43 @@ thereIsChars = True
 char = 0
 statesToGo = [{"number": 0, "words": None}]
 with open("language") as f:
-    statesToGo[0]["words"] = list(f.readlines())
+    statesToGo[0]["words"] = [(w.strip(), w.strip()) for w in list(f.readlines())]
 
 
 def printStructIntern(struct, d = 0):
-    print "  " * (d * 2),
-    print("switch (state) {")
+    print "  " * (d * 3),
+    print("switch (letra) {")
     for k in struct:
-        print "  " * (d * 2 + 1),
+        print "  " * (d * 3 + 1),
         print "case '%s':"%(k)
-        if struct[k]['final']:
-            print "  " * (d * 2 + 1),
-            print("return new Token(TokenType. , String.valueOf(palavra)); // retornar token para %s"%(struct[k]['words'][0]))
-        else:
-            print "  " * (d * 2 + 1),
+        for token in struct[k]['toCheck']:
+            print "  " * (d * 3 + 2),
+            print("if( palavra == \"%s\"){"%(token))
+            print "  " * (d * 3 + 3),
+            print("return new Token(TokenType.%s , String.valueOf(palavra)); // retornar token para %s"%(token, token))
+            print "  " * (d * 3 + 2),
+            print("}")
+        if struct[k]['final'] and len(struct[k]['words']) > 0:
+            token = struct[k]['words'][0][1]
+            print "  " * (d * 3 + 2),
+            print("if( palavra == \"%s\"){"%(token))
+            print "  " * (d * 3 + 3),
+            print("return new Token(TokenType.%s , String.valueOf(palavra)); // retornar token para %s"%(token, token))
+            print "  " * (d * 3 + 2),
+            print("}")
+        elif not struct[k]['final']:
+            print "  " * (d * 3 + 2),
             print("state = %i;"%(struct[k]['stateToGo']))
+        print "  " * (d * 3 + 2),
+        print "break;"
 
-    print "  " * (d * 2 + 1),
+    print "  " * (d * 3 + 1),
     print "default:"
-    print "  " * (d * 2 + 1),
+    print "  " * (d * 3 + 1),
     print "// Definir um erro"
-    print "  " * (d * 2 + 1),
+    print "  " * (d * 3 + 2),
     print "break;"
-    print "  " * (d * 2),
+    print "  " * (d * 3),
     print("}")
 
 def printStruct(struct, d = 0):
@@ -52,17 +66,29 @@ while(len(statesToGo) > 0) :
         switches[state["number"]] = {}
         sState = switches[state["number"]]
         for word in state["words"]:
-            if word[0] in sState:
-                if word[0] in newStates:
-                    newStates[word[0]]["words"].append(word[1:])
+            if word[0][0] in sState:
+                if word[0][0] in newStates:
+                    ans = newStates[word[0][0]]
+                    ans["words"].append((word[0][1:], word[1]))
                 else:
-                    states += 1
-                    newStates[word[0]] = {"words": [word[1:], sState[word[0]]['words'][0]], "number": states}
-                sState[word[0]]["final"] = False
-                sState[word[0]]["stateToGo"] = states
-                sState[word[0]]["words"].append(word)
+                    if(not word[0][1:]):
+                        sState[word[0][0]]["toCheck"].append(word[1])
+                    else:
+                        states += 1
+                        if(len(sState[word[0][0]]['words']) == 0):
+                            newStates[word[0][0]] = {"words": [(word[0][1:], word[1])], "number": states}
+                        else:
+                            newStates[word[0][0]] = {"words": [(word[0][1:], word[1]), (sState[word[0][0]]['words'][0][0][1:], sState[word[0][0]]['words'][0][1])], "number": states}
+                        sState[word[0][0]]["final"] = False
+                        sState[word[0][0]]["stateToGo"] = states
+                        sState[word[0][0]]["words"].append(word)
             else:
-                sState[word[0]] = {"final": True, "words": [word]}
+                sState[word[0][0]] = {"final": True, "words": [], "toCheck": []}
+                if(len(word[0]) == 1):
+                    sState[word[0][0]]["toCheck"].append(word[1])
+                else:
+                    sState[word[0][0]]["words"].append(word)
+
     statesToGo = newStates.values()
 
 printStruct(switches)
